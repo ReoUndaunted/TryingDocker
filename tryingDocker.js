7c7fa956d809e26d9e62b4ff8cfa00c15bf322eb
Raphael Ovonlen <reoundaunted@gmail.com>
2023-04-17 09:14:21 +0100

    trying out docker

---
+++ tryingDocker.js
@@ -0,0 +1,32 @@
+//! For checking error
+// @ts-nocheck
+// Write a message to the console.
+//! This code is for making print = console.log
+
+var print = function printToConsole(parameter) {
+  return printToConsole = console.log(parameter)
+}
+
+
+function add(a, b) {
+  return a + b;
+}
+
+function average(c, d, ___) {
+  return add(c, d) / 2;
+}
+print(average(10, 20))
+print(" hello world")
+
+print(average(45, 50))
+print(add(45, 50))
+
+switch (key) {
+case value:
+    
+    break;
+
+  default:
+    break;
+}
+
