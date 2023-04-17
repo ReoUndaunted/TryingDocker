ddb666bb1342bbad0df613d8c00a4806cc03daa4
Raphael Ovonlen <reoundaunted@gmail.com>
2023-04-17 17:40:24 +0100

    Publish to github

--- tryingDocker.js
+++ tryingDocker.js
@@ -20,5 +20,21 @@ print(" hello world")
 
 print(average(45, 50))
 print(add(45, 50))
+print("Today na the dey")
 
 
+var arr = ["car1", "car2", "car3", "car4", "car5"]
+for (let i = 0; i < arr.length; i++) {
+  // const element = arr[i];
+  const msg = " just one of them"
+  print(arr[i] + msg);
+}
+print(" ")
+
+// forEach(element => arr){
+//   print(_element + " just one of them")
+// }
+arr.forEach(function (b) {
+  print(b)
+}
+)
